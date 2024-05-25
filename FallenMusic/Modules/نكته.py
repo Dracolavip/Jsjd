bd3657408bc217fced78ef58ee09fbb49ed59d52
Dracolavip <nyndbnndnddn@gmail.com>
2024-05-25 18:36:55 +0300

    Update نكته.py

--- FallenMusic/Modules/نكته.py
+++ FallenMusic/Modules/نكته.py
@@ -1,6 +1,5 @@
 import asyncio
 from pyrogram import Client, filters
-from strings.filters import command
 from pyrogram.enums import ParseMode, ChatMemberStatus
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
